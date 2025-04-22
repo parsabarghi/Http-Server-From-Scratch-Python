9caea3a2585d8d1ba7c02fd31e3ca4d54b462687
parsa <parsabarqi@gmail.com>
2025-04-22 13:57:02 +0330

    first

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 import socket  # noqa: F401
 
+responses = {
+ "ok" : b"HTTP/1.1 200 OK\r\n\r\n",
+
+}
 
 def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("server is running...")
+    connection, address = server_socket.accept() # wait for client
+
+    with connection:
+        connection.sendall(responses["ok"])
 
 
 if __name__ == "__main__":
